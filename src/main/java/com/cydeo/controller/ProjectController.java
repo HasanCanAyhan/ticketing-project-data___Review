868b376578f083955e3435fae2f618baa7597778
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-09 13:27:38 +0100

    ticketing-project-data-review part5

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -131,14 +131,14 @@ public class ProjectController {
 
 
 
-    /*
+
     @GetMapping("/manager/complete/{projectCode}")
     public String managerCompleteProject(@PathVariable("projectCode") String projectCode) {
         projectService.complete(projectCode);
         return "redirect:/project/manager/project-status";
     }
 
-     */
+
 
 
 
